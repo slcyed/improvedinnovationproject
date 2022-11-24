7608bf2400680ca2b5cc51d0a905350274fcb036
slcyed <jonaselsass@yahoo.com>
2022-11-24 11:46:04 -0500

    first commit

---
+++ firstprediction.py
@@ -0,0 +1,46 @@
+import cv2 
+
+faceCascade = cv2.CascadeClassifier('trainings/haarcascade_frontalface_alt.xml')
+catCascade = cv2.CascadeClassifier('trainings/cascade.xml')
+facecount = 0
+catcount = 0
+
+# grab the reference to the webcam
+vs = cv2.VideoCapture(0)
+
+# keep looping
+while True:
+	# grab the current frame
+	ret, frame = vs.read()
+  
+	# if we are viewing a video and we did not grab a frame,
+	# then we have reached the end of the video
+	if frame is None:
+		break
+		
+	faces = faceCascade.detectMultiScale(frame)
+	cats = catCascade.detectMultiScale(frame)
+
+	for (x, y, w, h) in cats:
+		cv2.rectangle(frame, (x, y), (x+w, y+h), (255,0,0), 2)
+		catcount = catcount + 1
+	
+	for (x, y, w, h) in faces:
+		cv2.rectangle(frame, (x, y), (x+w, y+h), (255,0,0), 2)
+		facecount = facecount + 1
+	
+	cv2.putText(frame, 'Faces: '+str(facecount), (50,50), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0 ,0))
+	cv2.putText(frame, 'Cats: '+str(catcount), (50,40), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0 ,0))
+
+	# show the frame to our screen
+	cv2.imshow("Video", frame)
+	key = cv2.waitKey(1) & 0xFF
+
+	# if the 'q' or ESC key is pressed, stop the loop
+	if key == ord("q") or key == 27:
+		break
+	
+	facecount = 0
+	
+# close all windows
+cv2.destroyAllWindows()
