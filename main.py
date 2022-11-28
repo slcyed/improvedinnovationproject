8ce69c55bfe76d51329ac878a1788ddd4539d9af
slcyed <91488376+slcyed@users.noreply.github.com>
2022-11-28 14:13:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import cv2 
 
 faceCascade = cv2.CascadeClassifier('trainings/haarcascade_frontalface_alt.xml')
-catCascade = cv2.CascadeClassifier('trainings/cascade.xml')
 facecount = 0
-catcount = 0
 
 # grab the reference to the webcam
 vs = cv2.VideoCapture(0)
@@ -19,19 +17,13 @@ while True:
 		break
 		
 	faces = faceCascade.detectMultiScale(frame)
-	cats = catCascade.detectMultiScale(frame)
-
-	for (x, y, w, h) in cats:
-		cv2.rectangle(frame, (x, y), (x+w, y+h), (255,0,0), 2)
-		catcount = catcount + 1
 	
 	for (x, y, w, h) in faces:
 		cv2.rectangle(frame, (x, y), (x+w, y+h), (255,0,0), 2)
 		facecount = facecount + 1
 	
 	cv2.putText(frame, 'Faces: '+str(facecount), (50,50), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0 ,0))
-	cv2.putText(frame, 'Cats: '+str(catcount), (50,40), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0 ,0))
-
+	
 	# show the frame to our screen
 	cv2.imshow("Video", frame)
 	key = cv2.waitKey(1) & 0xFF
